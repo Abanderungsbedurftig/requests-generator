28c4ecce08e18ac8eeab6e0a5859afc5470e9678
unknown <gizmo.nas363@gmail.com>
2019-05-14 13:05:18 +0600

    update data.js

--- data/data.js
+++ data/data.js
@@ -1,9 +1,9 @@
 module.exports.data = [
-    {mode: 'vote', v: 16713}
+    {id: '1', data: '2'}
 ]
 
 module.exports.config = {
-    url: 'http://ktosomsk.ru/h78.php',  //required for all request
+    url: 'http://localhost:3000',  //required for all request
     count: 1,  //number of requests. required for getReq() and postReqEncoded()
     interval: 1000  //requests will be sent at this interval. required for getReq() and postReqEncoded()
 }
